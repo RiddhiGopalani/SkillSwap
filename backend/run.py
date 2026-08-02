21291cd46f83a7455e3a3d72ff3cf90a422f93b2
Riddhi Gopalani <riddhi.gopalani.18@gmail.com>
2026-08-02 18:56:09 +0530

    Prepare backend for deployment

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,13 @@
+import sys
+import os
+
+# Add current folder to sys.path so that 'app' module can be imported
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from app import create_app, socketio
+
+app = create_app()
+
 if __name__ == "__main__":
     socketio.run(
         app,
