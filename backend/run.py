c0a712c890684a2b8a9e57717f72c1ddd6208d25
Riddhi Gopalani <riddhi.gopalani.18@gmail.com>
2026-08-02 18:50:41 +0530

    Prepare backend for deployment

--- backend/run.py
+++ backend/run.py
@@ -1,13 +1,7 @@
-import sys
-import os
-
-# Add current folder to sys.path so that 'app' module can be imported
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from app import create_app, socketio
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Flask-SocketIO runs on port 5000 by default
-    socketio.run(app, debug=True, port=5000)
+if __name__ == "__main__":
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
